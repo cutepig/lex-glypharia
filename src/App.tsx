b0473eea2062ccbe5c165920bf0fbdcf570169a0
Alex Seidler <sopo.porsas@gmail.com>
2019-11-03 14:41:46 +0200

    Remove boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lex Glypharia</h1>
       </header>
+
+      <main>
+        <form
+          className="Form"
+          onChange={ev => console.log("form#onChange", ev)}
+          onSubmit={ev => (
+            ev.preventDefault(), console.log("form#onSubmit", ev)
+          )}
+        >
+          <label>
+            <span>Input:</span>
+            <input type="text" name="input" />
+          </label>
+        </form>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
